516558729fe7aa127c1f9990fcddb738fe891954
facuhomola <66138918+facuhomola@users.noreply.github.com>
2023-10-04 03:12:26 +0200

    Add files via upload

--- gimnasio/includes/header.php
+++ gimnasio/includes/header.php
@@ -1,19 +1,10 @@
-<div class="container-fluid">
-  <div class="row">
-      <div class="col-12">
-        <h3 class="text-center">Usuario:  
-          <?php
-            include('/gimnasio/bd/cn.php');
-            session_start();
-            $user = $_SESSION['user'];
-            echo "$user";
-          ?>
-        </h3>
-      </div>
-  </div>
-</div>
+<?php
+  include('/gimnasio/bd/cn.php');
+  session_start();
+  $user = $_SESSION['user'];
+?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg " style="background-color: #e3f2fd;">
   <div class="container-fluid">
     <a class="navbar-brand" href="/gimnasio/index.php">GYM</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,6 +20,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="bd/usuarios.php">Ver registros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Sesión: <?php echo $user ?></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/gimnasio/logout.php">Cerrar Sessión</a>
--- gimnasio/index.php
+++ gimnasio/index.php
@@ -27,6 +27,14 @@ if (!isset($user)) {
 ?>
 <!--Fin cabecera-->
 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-md-12 col-sm-12">
+            <h1 class="display-4 text-center"> SISTEMA GYM SL-VM </h1>
+        </div>
+    </div>
+</div>
+
 <section class="container-fluid">
     <div class="row">
         <div class="col-12 col-md-12 col-sm-12">
@@ -62,6 +70,45 @@ if (!isset($user)) {
     </div>
 </section>
     
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-md-12 col-sm-12">
+            <h3>Días - horarios - planes</h3>
+            <table class="table table-success table-striped">
+                <tr>
+                    <td><b>DÍA</b></td>
+                    <td><b>HORA</b></td>
+                    <td><b>PLAN</b></td>
+                </tr>
+                <tr>
+                    <td>Lunes - Miercóles - Viernes</td>
+                    <td>8:00 - 12:00</td>
+                    <td>Plan turno mañana - $4500</td>
+                </tr>
+                <tr>
+                    <td>Lunes - Miercóles - Viernes</td>
+                    <td>8:00 - 22:00</td>
+                    <td>Plan turno mañana/tarde - $6000</td>
+                </tr>
+                <tr>
+                    <td>Martes - Jueves</td>
+                    <td>8:00 - 12:00</td>
+                    <td>Plan 2 turno mañana - $3000</td>
+                </tr>
+                <tr>
+                    <td>Martes - Jueves</td>
+                    <td>8:00 - 22:00</td>
+                    <td>Plan 2 turno mañana/tarde - $3800</td>
+                </tr>
+                <tr>
+                    <td>Todos los días</td>
+                    <td>8:00 - 22:00</td>
+                    <td>Plan libre - $6700</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
 
 <!--Pie de pagina-->
 <?php    
